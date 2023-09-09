b7dbf3376ce04bc0849a4115e29c5f3c86773a80
LucasTalen <lucaspaula58227@outlook.com>
2023-09-09 00:30:00 -0300

    mexendo na API

--- scaneia.js
+++ scaneia.js
@@ -31,19 +31,20 @@ function retornaScan(codigoQr) {
 }
 
 
-function enviarCodigoQR(content){
-    let xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = function (){
-        if (xhr.readyState === 4 || xhr.status === 200){
-            let respostaJSON = JSON.parse(xhr.responseText);
-            document.getElementById('resultado').innerHTML = 'loja: ' + respostaJSON.loja + 'valor: '+ respostaJSON.valor;
-
-        }
-        else{
-            document.getElementById('resultado').innerHTML = 'Erro ao carregar dados';
-
-        }
-    }
-    xhr.open('GET','127.0.0.1:5000/api', true)
-    xhr.send()
+function enviarCodigoQR(){
+    fetch(`https://df87-2804-86d0-4015-2100-1059-7bc1-2581-5071.ngrok-free.app/api`)  // Altere a URL para a localização correta da sua API Flask
+  .then(response => response.json())
+  .then(data => {
+    // Aqui, 'data' conterá o JSON retornado pela sua API Flask
+    console.log(data);
+    
+    // Você pode agora manipular os dados como quiser, por exemplo, exibir na página HTML.
+    const loja = data.loja;
+    const preco = data.preco;
+    document.getElementById('loja').innerText = `Loja: ${loja}`;
+    document.getElementById('preco').innerText = `Preço: R$ ${preco}`;
+  })
+  .catch(error => {
+    console.error('Erro ao buscar dados:', error);
+  });
 }
