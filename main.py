c4c843d5a1b441e81b67a4c7b6b298e927278c16
LucasTalen <lucaspaula58227@outlook.com>
2023-09-08 22:21:07 -0300

    adicionando link na hora do scan

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
+
 #---------------------------------------
 #você tem que herdar o meu código js no progama e puxar a função 
 total = 0.0
-def extrairDados(total,url):
+def extrairDados(url):
     site = requests.get(url).text
     nome = BeautifulSoup(site, "html.parser")
     loja = nome.find("table").b.text
@@ -29,10 +32,11 @@ def extrairDados(total,url):
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/api")
 def hello_world():
-    resposta = extrairDados(total,url)
+    resposta = extrairDados("https://portalsped.fazenda.mg.gov.br/portalnfce/sistema/qrcode.xhtml?p=31230713574594030508650010005249781378283311|2|1|1|9CB061C0D016DBEDA8D27E6E20F4BA26025FF4A9")
     return resposta
 
 app.run(debug=True)
