942d76ee1b219e2f7ab454f738545aec03647c66
LucasTalen <lucaspaula58227@outlook.com>
2023-09-14 18:08:28 -0300

    resolvendo bug do filtro de id de cameras

--- scaneia.js
+++ scaneia.js
@@ -4,54 +4,53 @@ let cameraSelecionada = 0;
 // Pede permissão para acessara câmera, ele gera um erro caso a permisão seja negada
 Instascan.Camera.getCameras().then(cameras => {
     if (cameras.length > 0) {
-        var cameraDisponivel = cameras.filter(camera => camera.id)
+        cameraDisponivel = cameras.filter(camera => camera.id)
         console.log(cameraDisponivel)
         
         scanner.start(cameraDisponivel[1]);
         console.log(cameraSelecionada)
-
-        let trocaCamera = document.getElementById("trocaCamera")
-        trocaCamera.addEventListener('click', () => {
         cameraDisponivel = cameras.filter(camera => camera.id)
+    }
+});
 
-           cameraSelecionada = (cameraSelecionada + 1) % cameraDisponivel
-           console.log(cameraDisponivel)
-           console.log(cameraSelecionada)
-        
-            scanner.start(cameraDisponivel[cameraSelecionada]);
-        
-        })
-        
-        
-        let desliga = document.getElementById("desligar")
-        desliga.addEventListener('click', () => {
-            scanner.stop(cameras[posicao_camera])
-        })
-        
-        
-        
-        // Liga a câmera com o front-end, para a visualização do usuário
-        let scanner = new Instascan.Scanner({
-            video: document.getElementById('preview')
-        });
-        
-        // Verifica quando um QR code for escaneado
-        scanner.addListener('scan', function (content) {
-        
-            retornaScan(content);
-            content = ""
-        });
-        
-        // Função para passar o valor do código QR como uma função (retorna o valor do QR)
-        
-        function retornaScan(codigoQr) {
-            alert(codigoQr);
-            consumirAPI(codigoQr)
-        }
-        
+let trocaCamera = document.getElementById("trocaCamera")
+trocaCamera.addEventListener('click', () => {
+ 
+   cameraSelecionada = (cameraSelecionada + 1) % cameraDisponivel
+   console.log(cameraDisponivel)
+   console.log(cameraSelecionada)
 
-    }    
-});    
+    scanner.start(cameraDisponivel[cameraSelecionada]);
+
+})
+
+
+let desliga = document.getElementById("desligar")
+desliga.addEventListener('click', () => {
+    scanner.stop(cameras[posicao_camera])
+})
+
+
+
+// Liga a câmera com o front-end, para a visualização do usuário
+let scanner = new Instascan.Scanner({
+    video: document.getElementById('preview')
+});
+
+// Verifica quando um QR code for escaneado
+scanner.addListener('scan', function (content) {
+
+    retornaScan(content);
+    content = ""
+});
+
+// Função para passar o valor do código QR como uma função (retorna o valor do QR)
+
+function retornaScan(codigoQr) {
+    alert(codigoQr);
+    consumirAPI(codigoQr)
+}
+
 
 
 function consumirAPI(url) {
