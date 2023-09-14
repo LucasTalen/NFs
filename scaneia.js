f9405397ec2b0e35f72bfe69c3d89b6c03182397
LucasTalen <lucaspaula58227@outlook.com>
2023-09-14 15:43:49 -0300

    resolvendo bug do filtro de id de cameras

--- scaneia.js
+++ scaneia.js
@@ -5,7 +5,7 @@ alert('hello')
 Instascan.Camera.getCameras().then(cameras => {
     if (cameras.length > 0) {
         let cameraSelecionada = cameras.filter(camera => camera.id)
-        console.log(cameraSelecionada.id)
+        console.log(cameraSelecionada)
         alert(cameraSelecionada.id)
         scanner.start(cameraSelecionada[0]);
     }
